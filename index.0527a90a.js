01439b59f82cde2db006fbc832cfc9168522ba95
anatolytrue <97836186+anatolytrue@users.noreply.github.com>
2022-08-19 19:41:52 +0000

    Deploying to gh-pages from @ anatolytrue/goit-js-hw-11@a43f0ee687a0d57b8fcbf6d0062cd719ed30a019 🚀

--- index.0527a90a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#search-form");document.querySelector(".search-input"),document.querySelector(".search-submit"),document.querySelector(".load-more"),document.querySelector(".gallery");e.addEventListener("submit",clickSubmitBtn)}();
-//# sourceMappingURL=index.0527a90a.js.map
--- index.1c50497c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#search-form");document.querySelector(".search-input"),document.querySelector(".search-submit"),document.querySelector(".load-more"),document.querySelector(".gallery");e.addEventListener("submit",clickSubmitBtn);
-//# sourceMappingURL=index.1c50497c.js.map
